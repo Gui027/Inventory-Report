89f0e0c96e369d0d7144eb152e0878f5a95e163d
Guilherme <guilhermerg1234@gmail.com>
2022-08-14 15:02:13 -0300

    Req2

--- tests/product_report/test_product_report.py
+++ tests/product_report/test_product_report.py
@@ -1,5 +1,20 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
 
 def test_relatorio_produto():
-    pass  # Seu teste deve ser escrito aqui
+    product_rep = Product(
+        1,
+        "nome_do_produto",
+        "nome_da_empresa",
+        "21/02/2022",
+        "21/02/2025",
+        "numero_de_serie",
+        "armazenamento"
+    )
+    assert product_rep.__repr__() == (
+        f"O produto {product_rep.nome_do_produto}"
+        f" fabricado em {product_rep.data_de_fabricacao}"
+        f" por {product_rep.nome_da_empresa} com validade"
+        f" até {product_rep.data_de_validade}"
+        f" precisa ser armazenado {product_rep.instrucoes_de_armazenamento}."
+    )
